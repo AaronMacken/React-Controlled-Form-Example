d75e5c81dfc52681aad1962fc12ae2e1b7490243
Aaron Macken <aaron.a.macken@gmail.com>
2020-07-18 12:25:49 -0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      name: "",
+      age: "",
+      hobbies: "",
+    };
+  }
+
+  handleChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    alert(`${this.state.name} ${this.state.age} ${this.state.hobbies}`);
+    this.setState({
+      name: "",
+      age: "",
+      hobbies: "",
+    });
+  };
+
+  // console.log()
+  //
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <form onSubmit={this.handleSubmit}>
+            <input
+              type="text"
+              placeholder="name"
+              value={this.state.name}
+              name="name"
+              onChange={this.handleChange}
+            />
+            <input
+              type="text"
+              placeholder="age"
+              value={this.state.age}
+              name="age"
+              onChange={this.handleChange}
+            />
+            <input
+              type="text"
+              placeholder="hobbies"
+              value={this.state.hobbies}
+              onChange={this.handleChange}
+              name="hobbies"
+            />
+            <button type="submit">Submit Form</button>
+          </form>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
